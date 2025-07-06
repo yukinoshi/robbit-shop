493262781a1cb0dad9c8c90b5eec997e3477c7b0
yukinoshi <zouli8218@gmail.com>
2025-07-06 15:51:58 +0800

    完成了导航分类渲染

---
+++ src/stores/category.js
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import {getCategoryApi} from '@/apis/layout'
+export const useCategoryStore = defineStore('category', () => {
+  const categoryList = ref([])
+  const getCategory = async () => {
+    const res = await getCategoryApi()
+    categoryList.value = res.result
+  }
+  return { categoryList,getCategory }
+})
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,12 +2,18 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHead.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
+import {useCategoryStore} from '@/stores/category'
+import { onMounted } from 'vue'
+const categoryStore = useCategoryStore()
+onMounted(()=> categoryStore.getCategory())
 </script>
 
 <template>
   <LayoutNav></LayoutNav>
-  <router-view></router-view>
+  <LayoutFixed></LayoutFixed>
   <LayoutHeader></LayoutHeader>
+  <router-view></router-view>
   <LayoutFooter></LayoutFooter>
 </template>
 
