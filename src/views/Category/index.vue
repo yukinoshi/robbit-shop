d724ef79f4a6b4d16b430ec262569c388326efab
yukinoshi <zouli8218@gmail.com>
2025-07-07 15:28:43 +0800

    选中样式和分类商品列表渲染完成

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryApi } from '@/apis/category';
 import { getgetBannerApi } from '@/apis/home';
 import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import GoodsItem from '@/views/Home/components/GoodsItem.vue';
 const categoryData = ref({})
 const route = useRoute()
 const getCategory = async (id) => {
@@ -45,6 +46,25 @@ watch(
           </el-carousel-item>
         </el-carousel>
       </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -61,6 +81,7 @@ watch(
     height: 500px;
   }
 }
+
 .top-category {
   h3 {
     font-size: 28px;
