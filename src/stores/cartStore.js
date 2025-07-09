1fb4cc5ab66dcc1a0caead477411bf05a6725e0f
yukinoshi <zouli8218@gmail.com>
2025-07-09 18:03:01 +0800

    头部购物车基础渲染补充完成

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,8 +1,14 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 export const useCartStore = defineStore('cart',()=>{
   const cartList = ref([])
+  const sumCount = computed(()=>{
+    return cartList.value.reduce((sum,item)=>sum+=item.count,0)
+  })
+  const sumPrice = computed(()=>{
+    return cartList.value.reduce((sum,item)=>sum+=item.count * item.price,0)
+  })
   const addCart = (goods) => {
     const item = cartList.value.find((item) => goods.skuId===item.skuId)
     if (item) {
@@ -11,9 +17,15 @@ export const useCartStore = defineStore('cart',()=>{
       cartList.value.push(goods)
     }
   }
+  const delCart = (skuId) => {
+    cartList.value = cartList.value.filter((item)=>item.skuId!==skuId)
+  }
   return {
     cartList,
-    addCart
+    addCart,
+    delCart,
+    sumCount,
+    sumPrice
   }
 },{
   persist: true
