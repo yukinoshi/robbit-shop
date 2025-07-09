118b32e8f405e126d95af0a1f62be4601353348a
yukinoshi <zouli8218@gmail.com>
2025-07-09 22:36:06 +0800

    本地列表购物车基本数据渲染完成

--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { useCartStore } from '@/stores/cartStore';
-
 const CartStore = useCartStore()
 
 
@@ -35,7 +34,7 @@ const CartStore = useCartStore()
           <p>共 {{CartStore.sumCount}} 件商品</p>
           <p>&yen; {{CartStore.sumPrice.toFixed(2)}} </p>
         </div>
-        <el-button size="large" type="primary" >去购物车结算</el-button>
+        <el-button size="large" type="primary" @click="$router.push('/cartlist')">去购物车结算</el-button>
       </div>
     </div>
 </div>
